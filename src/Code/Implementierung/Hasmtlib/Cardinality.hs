4b6ec339afb9f700887bb3d7d6f20609144eaf18
bruderj15 <julian.bruder@outlook.com>
2024-09-14 17:02:10 +0200

    pure sqrt-kodierung

--- src/Code/Implementierung/Hasmtlib/Cardinality.hs
+++ src/Code/Implementierung/Hasmtlib/Cardinality.hs
@@ -1,19 +1,31 @@
-count' :: forall t f. (Functor f, Foldable f, Num (Expr t))
-    => Proxy t -> f (Expr BoolSort) -> Expr t
-count' _ = sum . fmap (\b -> ite b 1 0)
-
 count :: forall t f. (Functor f, Foldable f, Num (Expr t))
-    => f (Expr BoolSort) -> Expr t
-count = count' (Proxy @t)
+  => f (Expr BoolSort) -> Expr t
+count = sum . fmap (\b -> ite b 1 0)
 
-atMost  :: forall t f. (Functor f, Foldable f, Num (Expr t), Orderable (Expr t))
-    => Expr t -> f (Expr BoolSort) -> Expr BoolSort
-atMost  k = (<=? k) . count
+atMost  :: forall t f. (Functor f, Foldable f, KnownSMTSort t
+  , Num (HaskellType t), Ord (HaskellType t))
+  => Expr t -> f (Expr BoolSort) -> Expr BoolSort
+atMost 0 = nand
+atMost 1 = amoSqrt
+atMost k = (<=? k) . count
 
-atLeast :: forall t f. (Functor f, Foldable f, Num (Expr t), Orderable (Expr t))
-    => Expr t -> f (Expr BoolSort) -> Expr BoolSort
-atLeast k = (>=? k) . count
-
-exactly :: forall t f. (Functor f, Foldable f, Num (Expr t), Orderable (Expr t))
-    => Expr t -> f (Expr BoolSort) -> Expr BoolSort
-exactly k = (=== k) . count
+-- Pure product-encoding
+amoSqrt :: (Foldable f, Boolean b) => f b -> b
+amoSqrt xs
+  | length xs < 10 = amoQuad $ toList xs
+  | otherwise =
+      let n = toInteger $ length xs
+          p = ceiling $ sqrt $ fromInteger n
+          rows = splitEvery (fromInteger p) $ toList xs
+          columns = transpose rows
+          vs = or <$> rows
+          us = or <$> columns
+       in amoSqrt vs && amoSqrt us &&
+          and (
+            imap
+            (\j r -> and $
+                imap (\i x -> (x ==> us !! i) && (x ==> vs !! j)) r)
+            rows
+          )
+  where
+    splitEvery n = takeWhile (not . null) . map (take n) . iterate (drop n)
