d009a883e48e436c4fd36b90c4ba6f477fdbf9d5
bruderj15 <julian.bruder@outlook.com>
2024-10-07 12:23:26 +0200

    Zeilenabstand 1,5

--- src/Code/Einleitung/PressenlängeConstraintHaskell.hs
+++ src/Code/Einleitung/PressenlängeConstraintHaskell.hs
@@ -2,4 +2,4 @@ barLength :: [Bar (Expr RealSort)] -> Expr BoolSort
 barLength = all
               (\(b1,b2) -> (b1^.press === b2^.press) ==>
                 (b1^.length === b2^.length)
-              ) . binom
+              ) . binom 2
